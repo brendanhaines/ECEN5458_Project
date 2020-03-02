1baebc89a6c5461f8cb6e3fa9a14d6133929eba4
Brendan Haines <brendan.haines@gmail.com>
2020-03-02 16:30:00 -0700

    hopefully fix calibration issues

--- Software/python/hwtest.py
+++ Software/python/hwtest.py
@@ -42,6 +42,7 @@ def get_reflectivity(chan):
     adc_lock.acquire()
     for ii, io in enumerate(mux_io):
         io.value = mux[ii]
+    time.sleep(0.001)
     voltage = adc_mux.voltage
     adc_lock.release()
     return voltage
