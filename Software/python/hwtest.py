5ae0707162c19a34e2b4a765ee3b49e90fc579b1
Brendan Haines <brendan.haines@gmail.com>
2020-03-02 22:09:25 -0700

    change available tools on plots

--- Software/python/hwtest.py
+++ Software/python/hwtest.py
@@ -72,11 +72,11 @@ brightness_plot_source = ColumnDataSource(data=dict(sensor=brightness_idx, brigh
 time_plot_source = ColumnDataSource(data=dict(t=time_data[:,0], e=time_data[:,1], c=time_data[:,2]))
 
 # Set up plots
-brightness_plot = figure(plot_height=150, plot_width=400, x_range=[0, 7], y_range=[0, 1])
+brightness_plot = figure(plot_height=150, plot_width=400, x_range=[0, 7], y_range=[0, 1], tools="save")
 brightness_plot.line('sensor', 'brightness', source=brightness_plot_source, line_width=3)
 brightness_plot.circle('sensor', 'brightness', source=brightness_plot_source, size=8, fill_color="white", line_width=2)
 
-time_plot = figure(plot_height=400, plot_width=800, y_range=[-1, 1])
+time_plot = figure(plot_height=400, plot_width=800, y_range=[-1, 1], tools="crosshair,pan,reset,save,wheel_zoom")
 time_plot.line('t', 'e', source=time_plot_source, line_width=3, line_alpha=0.6, legend_label="e(t)")
 time_plot.line('t', 'c', source=time_plot_source, line_width=3, line_alpha=0.6, legend_label="c(t)", line_color = "green")
 
