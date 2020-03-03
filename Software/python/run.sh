cbff745300b64470d17e8ac84e35502309276f57
Brendan Haines <brendan.haines@gmail.com>
2020-03-03 00:02:46 -0700

    update run.sh to allow 192.168.4.1

--- Software/python/run.sh
+++ Software/python/run.sh
@@ -1,4 +1,4 @@
 # TODO: replace this origin with something generated from IP address
-export BOKEH_ALLOW_WS_ORIGIN=192.168.0.58:5006
+export BOKEH_ALLOW_WS_ORIGIN=192.168.4.1:5006
 
 bokeh serve hwtest.py
